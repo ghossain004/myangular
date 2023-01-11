68f603903c81b91da520f664bca75b355b5f63be
Md. Golam Hossain <105764760+hossain-dev@users.noreply.github.com>
2023-01-11 12:53:47 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './myCom/login/login.component';
+import { HomeComponent } from './myCom/home/home.component';
+import { TimelineComponent } from './myCom/timeline/timeline.component';
+import { ProfileComponent } from './myCom/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    TimelineComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
