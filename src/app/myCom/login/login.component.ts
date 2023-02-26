55455806c70525b2e2f87bb3015b613f0b8a1675
Md. Golam Hossain <105764760+ghossain004@users.noreply.github.com>
2023-02-27 01:26:36 +0600

    home 27.02.23

--- src/app/myCom/login/login.component.ts
+++ src/app/myCom/login/login.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/service/login.service';
+import { StorageService } from 'src/app/service/storage.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  form: any = {
+    username: null,
+    password: null
+  };
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  roles: string[] = [];
+
+  constructor(private loginService: LoginService, private storageService: StorageService) { }
+
+  ngOnInit(): void {
+    if (this.storageService.isLoggedIn()) {
+      this.isLoggedIn = true;
+      this.roles = this.storageService.getUser().roles;
+    }
+  }
+
+  onSubmit(): void {
+    const { username, password } = this.form;
+
+    this.loginService.login(username, password).subscribe({
+      next: data => {
+        this.storageService.saveUser(data);
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.storageService.getUser().roles;
+        this.reloadPage();
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    });
+  }
+
+  reloadPage(): void {
+    window.location.reload();
+  }
 
 }
--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -1,75 +1,50 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError, Observable, throwError } from "rxjs";
-import { Login } from "../model/login.model";
+import { Observable } from "rxjs";
+
+const AUTH_API = 'http://localhost:8080/api/auth/';
+
+
+const AUTH_API_REG = 'http://localhost:8080/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 
 export class LoginService {
     // private apiURL = "http://localhost:3000";
-    private apiURL = "http://localhost:8080/api";
+    // private apiURL = "http://localhost:8080/api";
 
-    httpOptions = {
-        headers: new HttpHeaders({
-            'Content-Type': 'application/json'
-        })
-    }
+    constructor(private http: HttpClient) {}
 
-    constructor(private httpClient: HttpClient) {}
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'signin',
+      {
+        username,
+        password,
+      },
+      httpOptions
+    );
+  }
 
-    getAll(): Observable<any> {
-  
-        return this.httpClient.get(this.apiURL + '/get', this.httpOptions)
-      
-        .pipe(
-          catchError(this.errorHandler)
-        )
-      }
+  register(username: string, email: string, password: string): Observable<any> {
+    return this.http.post(
+      AUTH_API_REG + 'signup',
+      {
+        username,
+        email,
+        password,
+      },
+      httpOptions
+    );
+  }
 
-      create(login:Login): Observable<any> {
-  
-        return this.httpClient.post(this.apiURL + '/signup', JSON.stringify(login), this.httpOptions)
-      
-        .pipe(
-          catchError(this.errorHandler)
-        )
-      }
-
-      find(id:number): Observable<any> {
-  
-        return this.httpClient.get(this.apiURL + '/find/' + id)
-      
-        .pipe(
-          catchError(this.errorHandler)
-        )
-      }
-
-      update(id:number, login:Login): Observable<any> {
-  
-        return this.httpClient.put(this.apiURL + '/update/' + id, JSON.stringify(post), this.httpOptions)
-     
-        .pipe( 
-          catchError(this.errorHandler)
-        )
-      }
-
-      delete(id:number){
-        return this.httpClient.delete(this.apiURL + '/delete/' + id, this.httpOptions)
-      
-        .pipe(
-          catchError(this.errorHandler)
-        )
-      }
-
-      errorHandler(error:any) {
-        let errorMessage = '';
-        if(error.error instanceof ErrorEvent) {
-          errorMessage = error.error.message;
-        } else {
-          errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
-        }
-        return throwError(errorMessage);
-     }
+  logout(): Observable<any> {
+    return this.http.post(AUTH_API + 'signout', { }, httpOptions);
+  }
 }
